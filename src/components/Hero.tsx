e9081bbe17bfaeddd04a95d5eda1e202535dc7b9
ingebrigtfb <ingebrigt03@gmail.com>
2026-02-25 13:54:30 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
       </div>
 
       {/* Wavy bottom divider */}
-      <div className="absolute bottom-0 left-0 right-0 z-20">
+      <div className="absolute -bottom-px left-0 right-0 z-20">
         <svg
           viewBox="0 0 1440 120"
           fill="none"
